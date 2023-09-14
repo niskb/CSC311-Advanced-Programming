24b5cd6d017de48d4e7dd63cd58a07fdc3d36f50
Brian Niski <niskibrian@hotmail.com>
2023-09-14 10:00:26 -0400

    Work in progress for Assignment 2
    Working on Assignment 2

---
+++ Assignments/Niski CSC 311 Assignment 2/Brian_Niski_Assignment2/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module niski.farmingdale.brian_niski_assignment2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens niski.farmingdale.brian_niski_assignment2 to javafx.fxml;
+    exports niski.farmingdale.brian_niski_assignment2;
+}
---
+++ Assignments/Niski CSC 311 Assignment 2/Brian_Niski_Assignment2/src/main/java/niski/farmingdale/brian_niski_assignment2/PrimaryController.java
@@ -0,0 +1,167 @@
+package niski.farmingdale.brian_niski_assignment2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
+public class PrimaryController {
+
+    /**
+     * Import primary.fxml containers/controls
+     */
+    @FXML
+    private ColorPicker colorPicker;
+    @FXML
+    private RadioButton lineRadioButton;
+    @FXML
+    private ToggleGroup shapeRadioButtonGroup;
+    @FXML
+    private RadioButton rectangleRadioButton;
+    @FXML
+    private RadioButton ovalRadioButton;
+    @FXML
+    private RadioButton freeDrawingRadioButton;
+    @FXML
+    private Slider slider;
+    @FXML
+    private Button clearCanvasButton;
+    @FXML
+    private Canvas canvas;
+    
+    /**
+     * Additional containers/controls needed
+     */
+    private Color penColor;
+    private GraphicsContext gc;
+    private byte selectedShapeCase;
+    private double startX;
+    private double startY;
+    private boolean isMouseClicked;
+    private int selectedSize;
+    
+    /**
+     * Initialize the JavaFX UI controls
+     */
+    public void initialize() {
+        // Method to get new value of the slider
+        slider.valueProperty().addListener((observable, newValue, oldValue) -> {
+            selectedSize = newValue.intValue();
+        });
+        
+        // Set default pen color to black
+        penColor = Color.BLACK;
+        
+        // Use the GraphicsContext Class to draw line, rectangle, oval shapes, and free drawing
+        gc = canvas.getGraphicsContext2D();
+        
+        // Initialize the pen color
+        gc.setStroke(penColor);
+        
+        // Set the default pen size to 1
+        selectedSize = 1;
+        // Initialize the pen size
+        gc.setLineWidth(selectedSize);
+    }
+    
+    /**
+     * Set the pen color based on action event of the color picker
+     * @param event 
+     */
+    @FXML
+    private void colorPickerHandler(ActionEvent event) {
+        penColor = colorPicker.getValue();
+    }
+
+    /**
+     * Set the selected shape tool based on action event of the group of the radio buttons
+     * If the lineRadioButton that is selected, set the selected shape case to 0
+     * If the rectangleRadioButton that is selected, set the selected shape case to 1
+     * If the ovalRadioButton that is selected, set the selected shape case to 2
+     * If the freeDrawingRadioButton that is selected, set the selected shape case to 3
+     * @param event 
+     */
+    @FXML
+    private void getSelectedShape(ActionEvent event) {
+        if(shapeRadioButtonGroup.getSelectedToggle() == lineRadioButton) {
+            selectedShapeCase = 0;
+        } else if(shapeRadioButtonGroup.getSelectedToggle() == rectangleRadioButton) {
+            selectedShapeCase = 1;
+        } else if(shapeRadioButtonGroup.getSelectedToggle() == ovalRadioButton) {
+            selectedShapeCase = 2;
+        } else if(shapeRadioButtonGroup.getSelectedToggle() == freeDrawingRadioButton) {
+            selectedShapeCase = 3;
+        }
+    }
+
+    /**
+     * Clear the canvas when the clear button is selected
+     * @param event 
+     */
+    @FXML
+    private void clearButtonHandler(ActionEvent event) {
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+    }
+
+    /**
+     * When the mouse is clicked in the canvas, get the starting x position and starting y position
+     * Prepare the GraphicsContext's line width, pen color, and begin path
+     * @param event 
+     */
+    @FXML
+    private void canvasOnMouseClicked(MouseEvent event) {
+        startX = event.getX();
+        startY = event.getY();
+        gc.setLineWidth(selectedSize);
+        gc.setStroke(penColor);
+        gc.beginPath();
+    }
+    
+    /**
+     * When the mouse is being dragged in the canvas, prepare the GraphicsContext's line width and pen color
+     * If the selected shape case is the freeDrawingRadioButton (3), the GraphicsContext is set for free drawing
+     * @param event 
+     */
+    @FXML
+    private void canvasOnMouseDragged(MouseEvent event) {
+        gc.setLineWidth(selectedSize);
+        gc.setStroke(penColor);
+        if(selectedShapeCase == 3) {
+            gc.lineTo(event.getX(), event.getY());
+            gc.stroke();
+        }
+    }
+
+    /**
+     * Get the x and y values of where the mouse is released
+     * If the selected shape case is lineRadioButton (0), the GraphicsContext is set for line drawing
+     * If the selected shape case is rectangleRadioButton (1), the GraphicsContext is set for rectangle drawing
+     * If the selected shape case is ovalRadioButton (2), the GraphicsContext is set for oval drawing
+     * @param event 
+     */
+    @FXML
+    private void canvasOnMouseReleased(MouseEvent event) {
+        double endX = event.getX();
+        double endY = event.getY();
+        if(selectedShapeCase == 0) {
+            gc.strokeLine(startX, startY, endX, endY);
+            gc.stroke();
+        } else if (selectedShapeCase == 1) {
+            // Use the absolute value so that the width and height is always positive for the rectangle
+            gc.strokeRect(startX, startY, Math.abs(endX - startX), Math.abs(endY - startY));
+            gc.stroke();
+        } else if (selectedShapeCase == 2) {
+            // Use the absolute value so that the width and height is always positive for the oval
+            gc.strokeOval(startX, startY,Math.abs(endX - startX), Math.abs(endY - startY));
+            gc.stroke();
+        }
+    }
+    
+}
