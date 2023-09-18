b58a05c702d08ab601129330ccbeb01ba7f7c453
Brian Niski <50961607+niskb@users.noreply.github.com>
2023-09-18 19:30:09 -0400

    Formatting and Use Switch statement
    Use Switch statement instead of if statements. Add new method isSmallerPosition() for checking the positions of the mouse on press and release. Format code. Update comments.

--- Assignments/Niski CSC 311 Assignment 2/Brian_Niski_Assignment2/src/main/java/niski/farmingdale/brian_niski_assignment2/PrimaryController.java
+++ Assignments/Niski CSC 311 Assignment 2/Brian_Niski_Assignment2/src/main/java/niski/farmingdale/brian_niski_assignment2/PrimaryController.java
@@ -15,7 +15,7 @@ import javafx.scene.paint.Color;
 public class PrimaryController {
 
     /**
-     * Import primary.fxml containers/controls
+     * Import primary.fxml containers/controls.
      */
     @FXML
     private ColorPicker colorPicker;
@@ -35,51 +35,53 @@ public class PrimaryController {
     private Button clearCanvasButton;
     @FXML
     private Canvas canvas;
-    
+
     /**
-     * Additional containers/controls needed
+     * Additional containers/controls needed.
      */
     private Color penColor;
     private GraphicsContext gc;
     private byte selectedShapeCase;
     private int selectedSize;
-    
+
     /**
-     * Mouse event coordinates
+     * Mouse event coordinate points on press and on release.
      */
     private double startX;
     private double startY;
     private double endX;
     private double endY;
-    
+
     /**
-     * Initialize the JavaFX UI controls
+     * Initialize the JavaFX UI controls.
      */
     public void initialize() {
-        // Method to get new value of the slider
+        // Method to set new value from the slider.
         slider.valueProperty().addListener((observable, oldValue, newValue) -> {
             selectedSize = newValue.intValue();
             gc.setLineWidth(selectedSize);
         });
-        
-        // Set default pen color to black
+
+        // Set default pen color to black.
         penColor = Color.BLACK;
-        
-        // Use the GraphicsContext Class to draw line, rectangle, oval shapes, and free drawing
+
+        // Use the GraphicsContext Class to draw line, rectangle, oval shapes, and free drawing.
         gc = canvas.getGraphicsContext2D();
-        
-        // Initialize the pen color
+
+        // Initialize the pen color.
         gc.setStroke(penColor);
-        
-        // Set the default pen size to 1
+
+        // Set the default pen size to 1.
         selectedSize = 1;
-        // Initialize the pen size
+
+        // Initialize the pen size.
         gc.setLineWidth(selectedSize);
     }
-    
+
     /**
-     * Set the pen color based on action event of the color picker
-     * @param event 
+     * Set the pen color based on action event of the color picker.
+     *
+     * @param event
      */
     @FXML
     private void colorPickerHandler(ActionEvent event) {
@@ -88,29 +90,32 @@ public class PrimaryController {
     }
 
     /**
-     * Set the selected shape tool based on action event of the group of the radio buttons
-     * If the lineRadioButton that is selected, set the selected shape case to 0
-     * If the rectangleRadioButton that is selected, set the selected shape case to 1
-     * If the ovalRadioButton that is selected, set the selected shape case to 2
-     * If the freeDrawingRadioButton that is selected, set the selected shape case to 3
-     * @param event 
+     * Set the selected shape tool based on action event of the group of the
+     * radio buttons. If the lineRadioButton is selected, set the selected shape
+     * case to 0. If the rectangleRadioButton is selected, set the selected
+     * shape case to 1. If the ovalRadioButton is selected, set the selected
+     * shape case to 2. If the freeDrawingRadioButton is selected, set the
+     * selected shape case to 3.
+     *
+     * @param event
      */
     @FXML
     private void getSelectedShape(ActionEvent event) {
-        if(shapeRadioButtonGroup.getSelectedToggle() == lineRadioButton) {
+        if (shapeRadioButtonGroup.getSelectedToggle() == lineRadioButton) {
             selectedShapeCase = 0;
-        } else if(shapeRadioButtonGroup.getSelectedToggle() == rectangleRadioButton) {
+        } else if (shapeRadioButtonGroup.getSelectedToggle() == rectangleRadioButton) {
             selectedShapeCase = 1;
-        } else if(shapeRadioButtonGroup.getSelectedToggle() == ovalRadioButton) {
+        } else if (shapeRadioButtonGroup.getSelectedToggle() == ovalRadioButton) {
             selectedShapeCase = 2;
-        } else if(shapeRadioButtonGroup.getSelectedToggle() == freeDrawingRadioButton) {
+        } else if (shapeRadioButtonGroup.getSelectedToggle() == freeDrawingRadioButton) {
             selectedShapeCase = 3;
         }
     }
 
     /**
-     * Clear the canvas when the clear button is selected
-     * @param event 
+     * Clear the canvas when the clear button is selected.
+     *
+     * @param event
      */
     @FXML
     private void clearButtonHandler(ActionEvent event) {
@@ -118,61 +123,90 @@ public class PrimaryController {
     }
 
     /**
-     * When the mouse is pressed in the canvas, get the starting x position and starting y position
-     * Prepare the GraphicsContext's line width, pen color, and begin path
-     * @param event 
+     * When the mouse is pressed in the canvas, get the starting x position and
+     * starting y position. If the selected shape case is the
+     * freeDrawingRadioButton (3), begin the path on the GraphicsContext.
+     *
+     * @param event
      */
     @FXML
     private void canvasOnMousePressed(MouseEvent event) {
         startX = event.getX();
         startY = event.getY();
-        if(selectedShapeCase == 3) {
+        if (selectedShapeCase == 3) {
             gc.beginPath();
         }
     }
-    
+
     /**
-     * When the mouse is being dragged in the canvas, prepare the GraphicsContext's line width and pen color
-     * If the selected shape case is the freeDrawingRadioButton (3), the GraphicsContext is set for free drawing
-     * @param event 
+     * While the mouse is being dragged, if the selected shape case is the
+     * freeDrawingRadioButton (3), the GraphicsContext is set for free drawing
+     * and draw continuously.
+     *
+     * @param event
      */
     @FXML
     private void canvasOnMouseDragged(MouseEvent event) {
-        if(selectedShapeCase == 3) {
+        if (selectedShapeCase == 3) {
             gc.lineTo(event.getX(), event.getY());
             gc.stroke();
         }
     }
 
     /**
-     * Get the x and y values of where the mouse is released
-     * If the selected shape case is lineRadioButton (0), the GraphicsContext is set for line drawing
-     * If the selected shape case is rectangleRadioButton (1), the GraphicsContext is set for rectangle drawing
-     * If the selected shape case is ovalRadioButton (2), the GraphicsContext is set for oval drawing
-     * If the selected shape case is freeDrawingRadioButton (3), the GraphicsContext is set for free drawing
-     * @param event 
+     * Get the x and y values of where the mouse is released. If the selected
+     * shape case is lineRadioButton (0), the GraphicsContext is set for line
+     * drawing. Draw line on release. If the selected shape case is
+     * rectangleRadioButton (1), the GraphicsContext is set for rectangle
+     * drawing. Draw rectangle on release. If the selected shape case is
+     * ovalRadioButton (2), the GraphicsContext is set for oval drawing. Draw
+     * oval on release. If the selected shape case is freeDrawingRadioButton
+     * (3), the GraphicsContext is set for free drawing. Close the path on the
+     * GraphicsContext.
+     *
+     * @param event
      */
     @FXML
     private void canvasOnMouseReleased(MouseEvent event) {
         endX = event.getX();
         endY = event.getY();
-        if(selectedShapeCase == 0) {
-            gc.strokeLine(startX, startY, endX, endY);
-        } else if (selectedShapeCase == 1) {
-            if((endX < startX) && (endY < startY)) {
-                gc.strokeRect(endX, endY, Math.abs(endX - startX), Math.abs(endY - startY));
-            } else {
-                gc.strokeRect(startX, startY, Math.abs(endX - startX), Math.abs(endY - startY));
-            }
-        } else if (selectedShapeCase == 2) {
-            if((endX < startX) && (endY < startY)) {
-                gc.strokeOval(endX, endY, Math.abs(endX - startX), Math.abs(endY - startY));
-            } else {
-                gc.strokeOval(startX, startY, Math.abs(endX - startX), Math.abs(endY - startY));
-            }
-        } else if (selectedShapeCase == 3) {
-            gc.closePath();
+        switch (selectedShapeCase) {
+            case 0:
+                gc.strokeLine(startX, startY, endX, endY);
+                break;
+            case 1:
+                if (isSmallerPosition()) {
+                    gc.strokeRect(endX, endY, Math.abs(endX - startX), Math.abs(endY - startY));
+                } else {
+                    gc.strokeRect(startX, startY, Math.abs(endX - startX), Math.abs(endY - startY));
+                }
+                break;
+            case 2:
+                if (isSmallerPosition()) {
+                    gc.strokeOval(endX, endY, Math.abs(endX - startX), Math.abs(endY - startY));
+                } else {
+                    gc.strokeOval(startX, startY, Math.abs(endX - startX), Math.abs(endY - startY));
+                }
+                break;
+            case 3:
+                gc.closePath();
+                break;
+            default:
+                break;
         }
     }
-    
+
+    /**
+     * Checks the position of the mouse on release is smaller than the position
+     * where it was initially pressed.
+     *
+     * @return
+     */
+    private boolean isSmallerPosition() {
+        if ((endX < startX) && (endY < startY)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
