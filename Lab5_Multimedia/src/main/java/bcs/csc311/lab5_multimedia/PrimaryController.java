62ad5531383f81030d018e44fbd07d44dab3a706
Brian Niski <50961607+niskb@users.noreply.github.com>
2023-09-20 11:58:53 -0400

    Change javafx-media dependency to version 13
    This will now play mp4 videos and also load JPG images into two separate views in separate tabs

--- Lab5_Multimedia/src/main/java/bcs/csc311/lab5_multimedia/PrimaryController.java
+++ Lab5_Multimedia/src/main/java/bcs/csc311/lab5_multimedia/PrimaryController.java
@@ -1,10 +1,14 @@
 package bcs.csc311.lab5_multimedia;
 
+import java.io.File;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 
 public class PrimaryController {
@@ -19,17 +23,32 @@ public class PrimaryController {
     private Button stopButton;
     @FXML
     private MediaView mediaView;
+    
+    private MediaPlayer mediaPlayer;
 
+    public void initialize() {
+//      Image image = new Image("file:image/372x279.jpg");
+        Image image = new Image(new File("image/372x279.jpg").toURI().toString()); 
+        imageView.setImage(image);
+        
+        Media media = new Media(new File("media/stock.mp4").toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+    }
+    
     @FXML
     private void playButtonHandler(ActionEvent event) {
+        mediaPlayer.play();
     }
 
     @FXML
     private void pauseButtonHandler(ActionEvent event) {
+        mediaPlayer.pause();
     }
 
     @FXML
     private void stopButtonHandler(ActionEvent event) {
+        mediaPlayer.stop();
     }
 
     
