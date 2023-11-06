8643f9321bb713d7d2fb41def1fc49e87e444c21
Brian Niski <50961607+niskb@users.noreply.github.com>
2023-11-06 12:03:42 -0500

    Lab13 More Query Work

--- Lab13_CoffeeDBDemo/src/main/java/bcs/csc/lab13_coffeedbdemo/Lab13_CoffeeDBDemo.java
+++ Lab13_CoffeeDBDemo/src/main/java/bcs/csc/lab13_coffeedbdemo/Lab13_CoffeeDBDemo.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -20,11 +21,12 @@ public class Lab13_CoffeeDBDemo {
     private final static String DB_URL = "jdbc:derby:CoffeeDB";
 
     public static void main(String[] args) {
-        createTable();
-        addRecordPPS();
+//        createTable();
+//        addRecordPPS();
         listRecord();
-        deleteRecord();
-        listRecord();
+//        deleteRecord();
+//        listRecord();
+        queryDatabaseWhileLoop();
     }
 
     public static void createTable() {
@@ -78,9 +80,9 @@ public class Lab13_CoffeeDBDemo {
             Statement stmt = conn.createStatement();
 
             String sqlStatement = "INSERT INTO Coffee "
-                    + "(ProdNum, Price, Description)"
-                    + " VALUES ('22-001', 8.65, 'Honduran Dark'),"
-                    + "('18-002', 14.85, 'Kona Dark')";
+                    + "(ProdNum, Description, Price)"
+                    + " VALUES ('22-001', 'Honduran Dark', 8.65),"
+                    + "('18-002', 'Kona Dark', 14.85)";
             int rows = stmt.executeUpdate(sqlStatement);
             System.out.println(rows + " records were added to the database.");
 
@@ -93,7 +95,7 @@ public class Lab13_CoffeeDBDemo {
             description = keyboard.nextLine();
             System.out.print("Enter the price of the record: ");
             price = keyboard.nextDouble();      // column names                      // variables
-            String query = "INSERT INTO Coffee (ProdNum, Price, Description) VALUES ('" + prodNum + "'," + price + ", '" + description + "')";
+            String query = "INSERT INTO Coffee (ProdNum, Description, Price) VALUES ('" + prodNum + "', '" + description + "', " + price + ")";
             rows = stmt.executeUpdate(query);
             System.out.println(rows + " record was added to the database.");
             keyboard.close();
@@ -109,7 +111,6 @@ public class Lab13_CoffeeDBDemo {
         try {
             Connection conn = DriverManager.getConnection(DB_URL);
             Statement stmt = conn.createStatement();
-
             Scanner keyboard = new Scanner(System.in);
             String prodNum, description;
             double price;
@@ -119,13 +120,14 @@ public class Lab13_CoffeeDBDemo {
             description = keyboard.nextLine();
             System.out.print("Enter the price of the record: ");
             price = keyboard.nextDouble();
-            String query = "INSERT INTO Coffee (ProdNum, Price, Description) VALUES (?, ?, ?)";
+            // Reminder: primaryKey cannot be duplicated
+            /*String query = "INSERT INTO Coffee (ProdNum, Description, Price) VALUES (?, ?, ?)";
             PreparedStatement pstmt = conn.prepareStatement(query);
             pstmt.setString(1, prodNum);
-            pstmt.setDouble(2, price);
-            pstmt.setString(3, description);
+            pstmt.setString(2, description);
+            pstmt.setDouble(3, price);
             int rows = pstmt.executeUpdate();
-            System.out.println(rows + " record was added to the database.");
+            System.out.println(rows + " record was added to the database.");*/
             keyboard.close();
             conn.close();
         } catch (SQLException ex) {
@@ -146,6 +148,92 @@ public class Lab13_CoffeeDBDemo {
             System.out.println(ex.getMessage());
             ex.printStackTrace();
         }
+    }
+
+    public static void queryDatabase() {
+        System.out.println("\nQuery Database =====");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a sql statement: ");
+        String query = keyboard.nextLine();
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL);
+            // By default, cursor is first row going forward
+            Statement stmt = conn.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+            ResultSet resultSet = stmt.executeQuery(query);
+            // To get the number of rows
+            resultSet.last();
+            int numRows = resultSet.getRow();
+            System.out.println("The number of rows: " + numRows);
+            resultSet.first();
+            // To get the meta data, columns and names of columns
+            ResultSetMetaData meta = resultSet.getMetaData();
+            int numCols = meta.getColumnCount();
+            String[] colNames = new String[numCols];
+            for (int i = 0; i < numCols; i++) {
+                colNames[i] = meta.getColumnLabel(i + 1);
+            }
+            String[][] tableData = new String[numRows][numCols];
+            for (int i = 0; i < numRows; i++) {
+                for (int j = 0; j < numCols; j++) {
+                    tableData[i][j] = resultSet.getString(j + 1);
+                    System.out.print(tableData[i][j] + "\t");
+                }
+                System.out.println();
+                resultSet.next();
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+        keyboard.close();
+    }
+
+    public static void queryDatabaseWhileLoop() {
+        System.out.println("\nQuery Database =====");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a sql statement: ");
+        String query = keyboard.nextLine();
+        while (!query.equals("exit")) {
+            try {
+                Connection conn = DriverManager.getConnection(DB_URL);
+                // By default, cursor is first row going forward
+                Statement stmt = conn.createStatement(
+                        ResultSet.TYPE_SCROLL_INSENSITIVE,
+                        ResultSet.CONCUR_READ_ONLY);
+                ResultSet resultSet = stmt.executeQuery(query);
+                // To get the number of rows
+                resultSet.last();
+                int numRows = resultSet.getRow();
+                System.out.println("The number of rows: " + numRows);
+                resultSet.first();
+                // To get the meta data, columns and names of columns
+                ResultSetMetaData meta = resultSet.getMetaData();
+                int numCols = meta.getColumnCount();
+                String[] colNames = new String[numCols];
+                for (int i = 0; i < numCols; i++) {
+                    colNames[i] = meta.getColumnLabel(i + 1);
+                }
+                String[][] tableData = new String[numRows][numCols];
+                for (int i = 0; i < numRows; i++) {
+                    for (int j = 0; j < numCols; j++) {
+                        tableData[i][j] = resultSet.getString(j + 1);
+                        System.out.print(tableData[i][j] + "\t");
+                    }
+                    System.out.println();
+                    resultSet.next();
+                }
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+                ex.printStackTrace();
+            }
+            System.out.print("Enter a sql statement: ");
+            query = keyboard.nextLine();
+        }
+        keyboard.close();
     }
 
 }
